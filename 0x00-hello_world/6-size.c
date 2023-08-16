6de40596d1314849bbed047065298934249051d4
root <mykelt026@gmail.com>
2023-08-16 02:13:38 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - this sdesribes the main function of the code
+ * Return: 0
+ 	*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	return (0);
+}
+
+
+100	#!/bin/bash
+gcc -S -masm=intel $CFILE
+
+
+101	#include <unistd.h>
+/**
+ 	* main - this prints exactly the input on it
+ * Return: always 1.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
