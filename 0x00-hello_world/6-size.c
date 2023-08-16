e2a8cfc423e2eae35781716f02f1426b8362593c
root <mykelt026@gmail.com>
2023-08-16 02:17:03 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,19 +18,3 @@ int main(void)
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
-
-
-100	#!/bin/bash
-gcc -S -masm=intel $CFILE
-
-
-101	#include <unistd.h>
-/**
- 	* main - this prints exactly the input on it
- * Return: always 1.
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
